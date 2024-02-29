c04e854576a706c44f8d97d17e0c5ae1e294a516
hyenci21 <159926659+hyenci21@users.noreply.github.com>
2024-02-29 16:57:47 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("**************");
+Console.WriteLine("PROGRAMA NO.4");
+Console.WriteLine("**************");
+Console.WriteLine("");
+int opis = 0;
+int numsecre = 21;
+do
+{
+
+    if (opis < numsecre)
+    {
+
+        Console.WriteLine("Su numero ingresado es menor al numero secreto");
+
+
+    }
+    else if (opis > numsecre)
+    {
+        Console.WriteLine("Su numero ingresado es mayor al numero secreto");
+    }
+    else
+    {
+        Console.WriteLine("¡FELICIDADES HA ENCONTRADO EL NUMERO SECRETO¡");
+    }
+
+
+
+}
+while (opis != 21);
+
+Console.WriteLine("TOCA UNA TECLA PARA VOLVER AL MENU PRINCIPAL");
